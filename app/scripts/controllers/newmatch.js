4d1d2f2c20860032c9708a261d35572b8ed8c6bd
Haseeb <hap12310@gmail.com>
2016-01-11 20:23:47 +0545

    new match become functional

--- app/scripts/controllers/newmatch.js
+++ app/scripts/controllers/newmatch.js
@@ -10,6 +10,19 @@
 angular.module('sample1App')
   .controller('NewmatchCtrl', function ($scope, firebaseService) {
  	
+ 	//for dropdown list
   	$scope.teamnames = firebaseService.getTeams();
+
+  	//testing 
+
+  	$scope.addmatch = function(unique,venue,teamformatch1,teamformatch2){
+  		var ref = new Firebase('https://burning-inferno-1731.firebaseio.com/matches');
+  		ref.child(unique).set({
+  			mtachvenue : venue,
+  			matchteam1 : teamformatch1,
+  			matchteam2 : teamformatch2
+  			}
+  		);
+  	};
 
   });
--- app/scripts/services/service.js
+++ app/scripts/services/service.js
@@ -1,49 +1,46 @@
 /*global angular */
 
 angular.module('sample1App')
-	.service('firebaseService', function ($firebaseArray, $firebaseObject) {
-		'use strict';
+    .service('firebaseService', function($firebaseArray, $firebaseObject) {
+        'use strict';
 
-		var service = {
-			baseUrl : 'https://burning-inferno-1731.firebaseio.com/'
-		};
+        var service = {
+            baseUrl: 'https://burning-inferno-1731.firebaseio.com/'
+        };
 
-		service.getTeams = function(){
-			var ref = new Firebase(service.baseUrl + "teams");
-			return $firebaseArray(ref);
-		};
+        service.getTeams = function() {
+            var ref = new Firebase(service.baseUrl + "teams");
+            return $firebaseArray(ref);
+        };
 
-		service.deletePlayer = function(player,team_id, player_id) {
-			var ref = new Firebase(service.baseUrl + "teams/" + team_id + "/players/" + player_id);
-			$firebaseObject.$remove(player);
-			return $firebaseArray(ref);
-		};
-		
-		service.getPlayerDetails = function(team_id,player_id){
-			var ref = new Firebase(service.baseUrl + "teams/" + team_id + "/players/" + player_id);
-			return $firebaseObject(ref);
-		};
+        service.deletePlayer = function(player, team_id, player_id) {
+            var ref = new Firebase(service.baseUrl + "teams/" + team_id + "/players/" + player_id);
+            $firebaseObject.$remove(player);
+            return $firebaseArray(ref);
+        };
 
-		service.getTeamDetails = function(teamid){
-			var ref = new Firebase(service.baseUrl + "teams/" + teamid ); 
-			return $firebaseObject(ref);
-		};
+        service.getPlayerDetails = function(team_id, player_id) {
+            var ref = new Firebase(service.baseUrl + "teams/" + team_id + "/players/" + player_id);
+            return $firebaseObject(ref);
+        };
 
-		service.addPlayer = function(team_id, player){
-			console.log(team_id);
-			console.log(player);
+        service.getTeamDetails = function(teamid) {
+            var ref = new Firebase(service.baseUrl + "teams/" + teamid);
+            return $firebaseObject(ref);
+        };
 
-			var ref = new Firebase(service.baseUrl + "teams/" + team_id + "/players");
-			 $firebaseArray(ref).$add(player);
-			 return true;
-		};
+        service.addPlayer = function(team_id, player) {
+            console.log(team_id);
+            console.log(player);
 
-
-
-		return service;
-    	
-
-	});
+            var ref = new Firebase(service.baseUrl + "teams/" + team_id + "/players");
+            $firebaseArray(ref).$add(player);
+            return true;
+        };
 
 
 
+        return service;
+
+
+    });
